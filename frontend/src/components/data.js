7c4646454c5979b313b63792cd832fbe8a327008
Aymen Hassini <hassiniaymen@yahoo.com>
2024-11-20 08:38:35 +0100

    adding location and time to events

--- frontend/src/components/data.js
+++ frontend/src/components/data.js
@@ -6,7 +6,8 @@ const initialProducts = [
         description:
             'Join us for an electrifying night as David Vendetta takes the stage! Experience his dynamic beats and chart-topping hits in an unforgettable concert. Get your tickets now for a night of non-stop dancing and incredible music!',
         brand: 'David Vendetta',
-        category: 'Music',
+        location: 'Tunis',
+        date: new Date(2024, 11, 20),
         price: 85,
         countInStock: 10,
         rating: 4.5,
@@ -18,7 +19,8 @@ const initialProducts = [
         image: 'assets/products/drift.jpg',
         description:
             'This Sunday, join us at the parking lot of the Carthage amphitheater for the Tunisia Drift Championship, organized by the Tunisian Automobile Federation and proudly sponsored by Agil Energy!',
-        brand: 'Tunisian Automobile Federation',
+        location: 'Tunis',
+        date: new Date(2024, 11, 20),
         category: 'Sport',
         price: 30,
         countInStock: 7,
@@ -31,7 +33,8 @@ const initialProducts = [
         image: 'assets/products/venom.jpg',
         description:
             'Watch Eddie Brock and Venom unite against a formidable new villain threatening humanity. With intense action and dark humor, this film promises an unforgettable experience. Get your tickets now!',
-        brand: 'Venom',
+        location: 'Tunis',
+        date: new Date(2024, 11, 20),
         category: 'Movie',
         price: 20,
         countInStock: 5,
@@ -44,7 +47,8 @@ const initialProducts = [
         image: 'assets/products/lellahom.jpg',
         description:
             'Join us for a night of hilarious entertainment featuring the best Tunisian comedians! Enjoy witty observations and charming stories that will leave you in stitches. Perfect for all ages, this is a comedy celebration you won’t want to miss. Get your tickets now!',
-        brand: 'Kaouthar Bardi',
+        location: 'Tunis',
+        date: new Date(2024, 11, 20),
         category: 'Comedy',
         price: 25,
         countInStock: 11,
@@ -57,7 +61,8 @@ const initialProducts = [
         image: 'assets/products/festival.jpg',
         description:
             'Experience the vibrant National Festival of Tunisian Theatre, showcasing local talent through diverse performances and workshops. Celebrate the creativity and cultural heritage of Tunisian theatre in this annual artistic event!',
-        brand: 'Ministry of Cultural Affairs of Tunisia',
+        location: 'Tunis',
+        date: new Date(2024, 11, 20),
         category: 'Theater',
         price: 50,
         countInStock: 7,
@@ -70,7 +75,8 @@ const initialProducts = [
         image: 'assets/products/jet.jpg',
         description:
             'Join us for an exciting showcase of innovative businesses and startups. Network with entrepreneurs, discover new products, and connect with industry leaders. Don’t miss your chance to be part of this vibrant event!',
-        brand: 'JET',
+        location: 'Tunis',
+        date: new Date(2024, 11, 20),
         category: 'Others',
         price: 15,
         countInStock: 0,
