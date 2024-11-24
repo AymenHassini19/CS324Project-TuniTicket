0409a72cbdb17df891dabdfd86cd003059363e4f
Aymen Hassini <hassiniaymen@yahoo.com>
2024-11-24 14:36:26 +0100

    adding customers page

--- frontend/src/pages/Customers.jsx
+++ frontend/src/pages/Customers.jsx
@@ -1,9 +1,91 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Accordion from "react-bootstrap/Accordion";
+import Button from "react-bootstrap/Button";
 
 const Customers = () => {
-  return (
-    <div>Customers</div>
-  )
-}
+  const url = "http://localhost:8000/api/users"; 
+  const [users, setUsers] = useState([]);
 
-export default Customers
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((res) => {
+        setUsers(res.data.users);
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const handleDelete = (userId) => {
+    if (window.confirm("Are you sure you want to delete this user?")) {
+      const token = localStorage.getItem('token');
+      axios
+        .delete(`${url}/${userId}`, {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        })
+        .then((res) => {
+          // Use the previous state to avoid issues with async state updates
+          setUsers((prevUsers) => prevUsers.filter((user) => user._id !== userId));
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+
+  return (
+    <div style={{ width: "80%", margin: "auto", marginTop: "40px" }}>
+      {users.map((user) => (
+        <Accordion key={user._id}> {/* Add key here for better performance */}
+          <Accordion.Item eventKey={user._id}> {/* Unique eventKey for each item */}
+            <Accordion.Header
+              style={{
+                backgroundColor: "lightgrey",
+                color: "white",
+                fontWeight: "bold",
+                padding: "10px 10px",
+                borderBottom: "1px solid #e3e3e3",
+                cursor: "pointer",
+              }}
+            >
+              {user.username.toUpperCase()}
+            </Accordion.Header>
+            <Accordion.Body
+              style={{
+                padding: "15px",
+                fontsize: "16px",
+                lineHeight: "1.5",
+                color: "#333",
+                display: "flex",
+                justifyContent: "space-between",
+              }}
+            >
+              <div>
+                <h5 style={{ marginBottom: "10px", color: "#555" }}>
+                  Email: {user.email}
+                </h5>
+                <h5>Age: {user.age}</h5>
+              </div>
+              <div>
+                <Button
+                  style={{ width: "80px" }}
+                  variant="danger"
+                  onClick={() => handleDelete(user._id)}
+                >
+                  Delete
+                </Button>
+              </div>
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+      ))}
+    </div>
+  );
+};
+
+export default Customers;
